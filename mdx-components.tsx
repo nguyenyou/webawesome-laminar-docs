1671b1d772163830ced59e7443f546832b500020
nguyenyou <tunt081295@gmail.com>
2025-11-07 19:29:15 +0700

    🚢 ship it

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,9 +1,11 @@
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
+import { Preview } from './components/Preview';
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
     ...components,
+    Preview: Preview,
   };
 }
--- previewPlugin.ts
+++ previewPlugin.ts
@@ -1,10 +1,33 @@
 import type { VFile } from "vfile";
 import { visit } from "unist-util-visit";
+import type { Plugin } from "unified";
+import type { Code, Root } from "mdast";
+import type { MdxJsxFlowElement } from "mdast-util-mdx-jsx";
 
-export function previewPlugin() {
-  return (tree: Node, file: VFile) => {
-    visit(tree, "code", (node: any) => {
+export const previewPlugin: Plugin<[any], Root> = () => {
+  return (tree, vfile) => {
+    visit(tree, "code", (node) => {
+      // hasVisited is a custom property
+      if ("hasVisited" in node) {
+        return;
+      }
+
+      if (node.lang && node.lang === "scala") {
+        // do not anything for pure mode
+        if (!node.meta?.includes('preview')) {
+          return;
+        }
+
+        // generate a module for the code block
         console.log("hi")
+      }
     });
   };
-}
+};
+// export function previewPlugin() {
+//   return (tree: Node, file: VFile) => {
+//     visit(tree, "code", node => {
+//         console.log("hi")
+//     });
+//   };
+// }
